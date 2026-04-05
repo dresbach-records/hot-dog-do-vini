4dc334233ebde347abedc30466347ecd2a9b62e9
Vinis Fullstack <dmgproductionsoficial@gmail.com>
2026-04-05 16:03:34 -0300

    chore: remove Vercel version and configure Vite build output directory and base path

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
   optimizeDeps: {
     exclude: ['react-router-dom'],
     include: ['cookie', 'set-cookie-parser']
   },
-
 })
